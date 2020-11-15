631de1d58323eaba403eb23c5a9b372b1f202144
KWAN YEOW CHYUAN <kwanyc95@outlook.com>
2020-11-15 21:46:15 +0800

    first commit

---
+++ FirewallRules/Execute.ps1
@@ -0,0 +1,26 @@
+﻿###Run this if not install Az module##############################################################################################################################################
+###Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi; Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'; rm .\AzureCLI.msi
+##################################################################################################################################################################################
+$Subscription = "<SubscriptionID>"
+$TenantID = "<TenantID>"
+$AzureFirewallName = "AZFirewallName"
+$ResourceGroupName = "AZFirewallRGName"
+$csv = Import-Csv .\FirewallRules.csv
+$location = "southeastasia"
+az login --allow-no-subscriptions -t $TenantID
+az account set -s $Subscription
+foreach ($line in $csv) {
+$RuleCollectionName = $line.RuleCollectionName
+$Priority = $line.Priority
+$Action = $line.Action
+$RuleName = $line.RuleName
+$Protocol = $line.Protocol
+$SourceAddresses = $line.SourceAddresses
+$DestinationAddresses = $line.DestinationAddresses
+$DestinationPorts = $line.DestinationPorts
+Write-Host "Creating Rule Collection " -NoNewline -ForegroundColor Red
+Write-Host $RuleCollectionName -NoNewline -ForegroundColor Red
+Write-Host "..." -ForegroundColor Red
+az network firewall network-rule create --collection-name $RuleCollectionName --destination-ports $DestinationPorts --firewall-name $AzureFirewallName --name $RuleName --protocols $Protocol --resource-group $ResourceGroupName --action $Action --destination-addresses $DestinationAddresses --priority $Priority --source-addresses $SourceAddresses --only-show-errors
+}
+Write-Host "Finish Create all Firewall Rules!" -ForegroundColor Blue
---
+++ Readme.txt
@@ -0,0 +1,12 @@
+1) Open FirewallRules.csv.
+2) Add on required firewall rules.
+3) Open Execute.ps1.
+4) Change <SubscriptionID> in line 4.
+5) Change <TenantID> in line 5.
+6) Change <AZFirewallName> in line 6.
+6) Change <AZFirewallRGName> in line 6.
+7) Execute Execute.ps1.
+8) Login.
+9) Verify after "Finish Create all Azure Firewall Rules!".
+
+Note: Install Azure CLI if not install any Az module. (Refer to line 2).
